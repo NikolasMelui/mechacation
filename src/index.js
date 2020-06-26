7ce2176b01078400aa6b398f6db1d801bc785986
nikolasmelui <slak@samaradom.ru>
2020-06-26 17:50:12 +0400

    Update the index.js file

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 'use strict';
 
-const path = require('path');
-
-const Client = require('./Client');
+const Client = require('./Client/Client');
+const ClientConfig = require('./Client/ClientConfig');
 
 const { printCourses, printEvents } = require('./helpers');
 
-const {
-  SCOPES,
-  CREDENTIALS_FILE_PATH,
-  // SERVICE_FILE_PATH,
-  TOKEN_FILE_PATH,
-} = require('./config');
-const credentialsFilePath = path.resolve(__dirname, CREDENTIALS_FILE_PATH);
-// const serviceFilePath = path.resolve(__dirname, SERVICE_FILE_PATH);
-const tokenFilePath = path.resolve(__dirname, TOKEN_FILE_PATH);
+const { SCOPES } = require('./secrets/config');
 
 (async () => {
   try {
-    const client = new Client(credentialsFilePath, tokenFilePath, SCOPES);
+    const clientConfig = new ClientConfig(SCOPES);
+    const client = new Client(clientConfig);
     await client.init();
     await client.authorize();
 
