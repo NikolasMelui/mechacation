3e6093a6d549814cc1e9992109e111843ed2193f
nikolasmelui <slak@samaradom.ru>
2020-06-26 17:47:35 +0400

    Move Client.js file to the Client directory

--- src/Client/Client.js
+++ src/Client/Client.js
@@ -1,32 +1,39 @@
+'use strict';
+
 const fs = require('fs').promises;
+const path = require('path');
 const readline = require('readline');
 const { google } = require('googleapis');
 
 /**
- * Client class ('main' class of the application)
- *
  * @name Client
+ * @type Class
+ * @description Main application class
  */
 class Client {
   /**
-   * @param {string} credentialsPath
-   * @param {string} tokenPath
-   * @param {String[]} scope
+   * @type constructor
+   * @param clientConfig ClientConfig instance
    */
-  constructor(credentialsPath, tokenPath, scope) {
-    this.credentialsPath = credentialsPath;
-    this.tokenPath = tokenPath;
-    this.scope = scope;
+  constructor(clientConfig) {
+    this.secretsPath = path.resolve(__dirname, '../secrets');
+    this.credentialsFilePath = path.resolve(
+      this.secretsPath,
+      'credentials.json',
+    );
+    this.serviceFilePath = path.resolve(this.secretsPath, 'service.json');
+    this.tokenFilePath = path.resolve(this.secretsPath, 'token.json');
+    this.scope = clientConfig.scope;
   }
 
   /**
-   * Initialize the 'main' instances of the application by reading the credentials
-   *
    * @name init
+   * @type method
+   * @description Initialize the instance of the application
    */
   async init() {
     try {
-      const credentials = await this.getCredentials(this.credentialsPath);
+      const credentials = await this.getFileData(this.credentialsFilePath);
 
       const { client_id, client_secret, redirect_uris } = credentials.installed;
 
@@ -51,40 +58,29 @@ class Client {
   }
 
   /**
-   * Get the credentialsPath string, read the `credentials.json` file and return the JSON
-   *
-   * @name getCredentials
-   * @param {string} credentialsPath
-   * @returns {Object}
+   * @name getFileData
+   * @type method
+   * @description Get the file path (string), read, parse and return JSON file data
+   * @param {string} filePath
+   * @returns {Object} fileData
    */
-  async getCredentials(credentialsPath) {
-    return JSON.parse(await fs.readFile(credentialsPath));
+  async getFileData(filePath) {
+    return JSON.parse(await fs.readFile(filePath));
   }
 
   /**
-   * Get the tokenPath string, read the `token.json` file and return the JSON
-   *
-   * @name getToken
-   * @param {string} tokenPath
-   * @returns {Object}
-   */
-  async getToken(tokenPath) {
-    return JSON.parse(await fs.readFile(tokenPath));
-  }
-
-  /**
-   * Read the token data and set it into oAuth2Client credentials
-   *
    * @name authorize
+   * @type method
+   * @description Read the token data and set it into oAuth2Client credentials
    */
   async authorize() {
     try {
       try {
-        const token = await this.getToken(this.tokenPath);
+        const token = await this.getFileData(this.tokenFilePath);
         this.oAuth2Client.setCredentials(token);
       } catch (error) {
         await this.getNewToken();
-        const token = await this.getToken(this.tokenPath);
+        const token = await this.getFileData(this.tokenFilePath);
         this.oAuth2Client.setCredentials(token);
       }
     } catch (error) {
@@ -96,6 +92,9 @@ class Client {
    * Get and store new token after prompting for user authorization
    *
    * @name getNewToken
+   * @type method
+   * @description Get and store new token after prompting for user authorization
+   * @returns {Promise}
    */
   async getNewToken() {
     const authUrl = this.oAuth2Client.generateAuthUrl({
@@ -117,8 +116,8 @@ class Client {
           const token = JSON.stringify(
             (await this.oAuth2Client.getToken(code)).tokens,
           );
-          await fs.writeFile(this.tokenPath, token);
-          console.log('Token stored into ', this.tokenPath);
+          await fs.writeFile(this.tokenFilePath, token);
+          console.log('Token stored into ', this.tokenFilePath);
           resolve();
         } catch (error) {
           console.error('Error retrieving access token', error);
@@ -129,8 +128,9 @@ class Client {
   }
 
   /**
-   * Get the courses
-   *
+   * @name getCourses
+   * @type method
+   * @description Return courses
    * @returns {Object} courses
    */
   async getCourses() {
@@ -147,8 +147,9 @@ class Client {
   }
 
   /**
-   * Get the events
-   *
+   * @name getEvents
+   * @type method
+   * @description Return events
    * @returns {Object} events
    */
   async getEvents() {
