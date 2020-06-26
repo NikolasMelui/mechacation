1210da2a51781957a747e6861de0b1c6fbc850c7
nikolasmelui <slak@samaradom.ru>
2020-06-26 17:49:11 +0400

    Update the example config file

--- src/config.example.js
+++ src/config.example.js
@@ -1,6 +1,4 @@
 // const config = {
-//   CREDENTIALS_PATH: './google-secrets/credentials.json',
-//   TOKEN_PATH: './google-secrets/token.json',
 //   SCOPES: ['https://www.googleapis.com/auth/classroom.courses'],
 // };
 
